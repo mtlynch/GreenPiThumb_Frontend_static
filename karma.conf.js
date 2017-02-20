89c17f4808e1df4178c083b59cb2c2ba7d2e4af7
Michael Lynch <mtlynch@google.com>
2017-02-20 07:44:09 -0500

    Updating to look like angular-seed

---
+++ karma.conf.js
@@ -0,0 +1,44 @@
+// Karma configuration
+// Generated on Sat Feb 18 2017 09:33:16 GMT-0500 (EST)
+
+module.exports = function(config) {
+  config.set({
+
+    basePath: './app',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'components/**/*.js'
+    ],
+
+    exclude: [
+    ],
+
+    preprocessors: {
+      'app/*.js': ['coverage']
+    },
+
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
+
+    port: 8000,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: false,
+
+    browsers: ['PhantomJS2'],
+
+    concurrency: Infinity
+  });
+};
