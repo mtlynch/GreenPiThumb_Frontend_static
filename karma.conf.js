d9c23cb12ffeb81351ea44ee517d050d8d535d82
Michael Lynch <mtlynch@google.com>
2017-02-24 21:37:13 -0500

    Unit tests working

--- karma.conf.js
+++ karma.conf.js
@@ -12,11 +12,11 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'app.js',
       'bower_components/d3/d3.js',
       'components/**/*.js',
       'services/**/*.js',
-      'directives/**/*.js',
-      'app.js'
+      'directives/**/*.js'
     ],
 
     exclude: [
